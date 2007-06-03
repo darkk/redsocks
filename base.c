b644e67ecbea285fe584ffa0cd5523cf499b77a6
darkk <darkk@f324f89c-ff31-0410-8eac-c7e7ec545cec>
2007-06-03 13:21:53 +0000

    Retabbed file a bit.

--- base.c
+++ base.c
@@ -1,12 +1,12 @@
 /* $Id$ */
 
-       #include <sys/types.h>
-       #include <sys/stat.h>
-       #include <fcntl.h>
-       #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <string.h>
-       #include <pwd.h>
-       #include <grp.h>
+#include <pwd.h>
+#include <grp.h>
 #include "log.h"
 #include "main.h"
 #include "parser.h"
