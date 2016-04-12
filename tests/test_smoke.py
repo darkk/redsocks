7b1f67b395682123aeb9eb6b79b2401967b45119
Leonid Evdokimov <leon@darkk.net.ru>
2016-04-12 23:47:11 +0300

    Test that `nonce` reuse in http-digest actually works

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -1,3 +1,4 @@
+from functools import partial
 from subprocess import check_call
 import time
 
@@ -35,8 +36,10 @@ LATENCY = {
     'regw_direct':      2 * RTT,
 }
 
-def http_ping(vm):
+def heatup(vm):
     vm.do('curl -o /dev/null http://10.0.1.80/') # heatup L2 and auth caches
+
+def http_ping(vm):
     s = vm.do('curl -sS -w %{{time_connect}}/%{{time_total}}/%{{http_code}}/%{{size_download}} -o /dev/null http://10.0.1.80/')
     connect, total, code, size = s.split('/')
     connect, total, code, size = float(connect) * 1000, float(total) * 1000, int(code), int(size)
@@ -45,12 +48,27 @@ def http_ping(vm):
 @pytest.mark.parametrize('tank', conftest.TANKS.keys())
 def test_latency_tank(slow_net, tank):
     vm = slow_net.vm['tank%d' % conftest.TANKS[tank]]
+    heatup(vm)
     connect, total, code, size = http_ping(vm)
     assert code == 200 and size == 612
     assert connect < 0.005 and LATENCY[tank]-RTT*.2 < total and total < LATENCY[tank]+RTT*.2
 
 def test_latency_regw(slow_net):
     vm, tank = slow_net.vm['regw'], 'regw_direct'
+    heatup(vm)
     connect, total, code, size = http_ping(vm)
     assert code == 200 and size == 612
     assert RTT*.8 < connect and connect < RTT*1.2 and LATENCY[tank]-RTT*.2 < total and total < LATENCY[tank]+RTT*.2
+
+def test_nonce_reuse(slow_net):
+    """ nonce reuse works and has no latency penalty """
+    tank = 'connect_digest'
+    vm = slow_net.vm['tank%d' % conftest.TANKS[tank]]
+    heatup(vm)
+    begin = time.time()
+    s = conftest.pmap([partial(http_ping, vm) for _ in range(5)])
+    total_sum = time.time() - begin
+    for connect, total, code, size in s:
+        assert code == 200 and size == 612
+        assert connect < 0.005 and LATENCY[tank]-RTT*.2 < total and total < LATENCY[tank]+RTT*.2
+        assert total_sum < total * 1.5
